330c9875646900832e5e8a4a7d279b481bf9baa1
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2022-10-26 11:02:46 -0400

    Spectral vs greedy comparison

--- launch/single_machine_experiments/kitti_stereo.launch.py
+++ launch/single_machine_experiments/kitti_stereo.launch.py
@@ -19,13 +19,13 @@ def launch_setup(context, *args, **kwargs):
     # Params
     nb_robots = int(LaunchConfiguration('nb_robots').perform(context))
     dataset = "KITTI" + LaunchConfiguration('sequence').perform(context)
-    startup_delay_s = LaunchConfiguration('startup_delay_s').perform(context)  
-    bag_delay_s = LaunchConfiguration('bag_delay_s').perform(context)  
+    robot_delay_s = LaunchConfiguration('robot_delay_s').perform(context)  
+    launch_delay_s = LaunchConfiguration('launch_delay_s').perform(context)  
     rate = float(LaunchConfiguration('rate').perform(context))
 
     # Ajust value according to rate
-    startup_delay_s = float(startup_delay_s) / rate
-    bag_delay_s = float(bag_delay_s) / rate
+    robot_delay_s = float(robot_delay_s) / rate
+    launch_delay_s = float(launch_delay_s) / rate
 
     cslam_processes = []
     bag_processes = []
@@ -52,7 +52,7 @@ def launch_setup(context, *args, **kwargs):
 
         bag_file = os.path.join(
             get_package_share_directory("cslam_experiments"), "data",
-            dataset + "-" + str(i))
+            dataset + "_" + str(nb_robots) + "robots", dataset + "-" + str(i))
 
         bag_proc = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -98,19 +98,19 @@ def launch_setup(context, *args, **kwargs):
     for i in range(nb_robots):
         schedule.append(PushLaunchConfigurations())
         schedule.append(
-            TimerAction(period=float(startup_delay_s) * i,
+            TimerAction(period=float(robot_delay_s) * i,
                         actions=[cslam_processes[i]]))
         schedule.append(PopLaunchConfigurations())
         schedule.append(PushLaunchConfigurations())
         schedule.append(
-            TimerAction(period=float(startup_delay_s) * i,
+            TimerAction(period=float(robot_delay_s) * i,
                         actions=[odom_processes[i]]))
         schedule.append(PopLaunchConfigurations())        
 
     for i in range(nb_robots):
         schedule.append(PushLaunchConfigurations())
         schedule.append(
-            TimerAction(period=float(startup_delay_s) * i + float(bag_delay_s),
+            TimerAction(period=float(robot_delay_s) * i + float(launch_delay_s),
                         actions=[bag_processes[i]]))
         schedule.append(PopLaunchConfigurations())
 
@@ -124,12 +124,12 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('nb_robots', default_value='2'),
         DeclareLaunchArgument('sequence', default_value='00'),
-        DeclareLaunchArgument('startup_delay_s', default_value='120', description="Delay between launching each robot. Ajust depending on the computing power of your machine."),
-        DeclareLaunchArgument('bag_delay_s', default_value='10', description="Delay between launching the bag and the robot. In order to let the robot initialize properly and not loose the first bag data frames."),
+        DeclareLaunchArgument('robot_delay_s', default_value='240', description="Delay between launching each robot. Ajust depending on the computing power of your machine."),
+        DeclareLaunchArgument('launch_delay_s', default_value='10', description="Delay between launching the bag and the robot. In order to let the robot initialize properly and not loose the first bag data frames."),
         DeclareLaunchArgument('config_file',
                               default_value='kitti_stereo.yaml',
                               description=''),
-        DeclareLaunchArgument('rate', default_value='0.2'),
+        DeclareLaunchArgument('rate', default_value='0.5'),
         DeclareLaunchArgument('enable_simulated_rendezvous', default_value='true'),
         DeclareLaunchArgument('rendezvous_config', default_value='kitti00_2robots.config'),
         OpaqueFunction(function=launch_setup)
