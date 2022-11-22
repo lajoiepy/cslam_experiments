47e995c688d68e85ff96d97ae3446d8cd34cd9ce
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2022-11-22 11:31:08 -0500

    Add S3E msg conversions and calibration publishing

---
+++ cslam_experiments/publish_stereo_calibration_s3e.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# Loop Closure Detection service
+# Abstraction to support multiple implementations of loop closure detection for benchmarking
+
+import rclpy
+from rclpy.node import Node
+from rclpy.clock import Clock
+from sensor_msgs.msg import Image, CameraInfo
+import yaml
+
+class CalibrationPublisher(Node):
+    """ Publish stereo calibration """
+
+    def __init__(self):
+        """Initialization and parameter parsing"""
+        super().__init__('loop_closure_detection')
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[('robot_id', None),
+                        ('frontend.stereo_calibration_file', None),
+                        ])
+        self.params = {}
+        self.params['robot_id'] = self.get_parameter('robot_id').value
+        self.params["frontend.stereo_calibration_file"] = self.get_parameter(
+            'frontend.stereo_calibration_file').value
+
+        # Subscribe to the stereo images
+        self.sub_left = self.create_subscription(
+            Image, '/r{}/stereo_camera/left/image_color'.format(self.params['robot_id']), self.left_callback, 10)
+        self.sub_right = self.create_subscription(  
+            Image, '/r{}/stereo_camera/right/image_color'.format(self.params['robot_id']), self.right_callback, 10)
+
+        # Publish the stereo calibration
+        self.pub_left = self.create_publisher(CameraInfo, '/r{}/stereo_camera/left/camera_info'.format(self.params['robot_id']), 10)
+        self.pub_right = self.create_publisher(CameraInfo, '/r{}/stereo_camera/right/camera_info'.format(self.params['robot_id']), 10)
+
+        # Read the calibration file (yaml)
+        self.calib_info = yaml.load(open(self.params["frontend.stereo_calibration_file"], 'r'), Loader=yaml.FullLoader)
+        self.get_logger().info("Loaded calibration file: {}".format(self.params["frontend.stereo_calibration_file"]))
+        self.left_calibration = CameraInfo()
+        self.right_calibration = CameraInfo()
+        self.left_calibration.height = self.calib_info['LEFT.height']
+        self.left_calibration.width = self.calib_info['LEFT.width']
+        self.left_calibration.distortion_model = "plumb_bob"
+        self.left_calibration.d = self.calib_info['LEFT.D']['data']
+        self.left_calibration.k = self.calib_info['LEFT.K']['data']
+        self.left_calibration.r = self.calib_info['LEFT.R']['data']
+        self.left_calibration.p = self.calib_info['LEFT.P']['data']
+
+        self.right_calibration.height = self.calib_info['RIGHT.height']
+        self.right_calibration.width = self.calib_info['RIGHT.width']
+        self.right_calibration.distortion_model = "plumb_bob"
+        self.right_calibration.d = self.calib_info['RIGHT.D']['data']
+        self.right_calibration.k = self.calib_info['RIGHT.K']['data']
+        self.right_calibration.r = self.calib_info['RIGHT.R']['data']
+        self.right_calibration.p = self.calib_info['RIGHT.P']['data']
+
+    def left_callback(self, msg):
+        """Publish the calibration"""
+        self.left_calibration.header = msg.header
+        self.pub_left.publish(self.left_calibration)
+
+    def right_callback(self, msg):
+        """Publish the calibration"""
+        self.right_calibration.header = msg.header
+        self.pub_right.publish(self.right_calibration)
+
+if __name__ == '__main__':
+
+    rclpy.init(args=None)
+    cp = CalibrationPublisher()
+    cp.get_logger().info('Initialization done.')
+    rclpy.spin(cp)
+    rclpy.shutdown()
---
+++ launch/sensors/bag_s3e.launch.py
@@ -0,0 +1,69 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.actions import SetParameter
+
+
+def launch_setup(context, *args, **kwargs):
+
+    return [
+        DeclareLaunchArgument('bag_file', default_value='', description=''),
+        DeclareLaunchArgument('namespace', default_value='/r', description=''),
+        DeclareLaunchArgument('rate', default_value='1.0', description=''),
+        DeclareLaunchArgument('bag_start_delay',
+                              default_value='5.0',
+                              description=''),
+        TimerAction(
+            period=LaunchConfiguration('bag_start_delay'),
+            actions=[
+                ExecuteProcess(
+                    cmd=[
+                        'ros2', 'bag', 'play',
+                        LaunchConfiguration('bag_file').perform(context), '-r',
+                        LaunchConfiguration('rate'), '--remap',
+                        '/Alpha/imu/data:=' +
+                        LaunchConfiguration('namespace').perform(context) +
+                        '0/imu/data'
+                    ],
+                    name='bag',
+                    output='screen',
+                )
+            ]),
+        Node(package='image_transport',
+             executable='republish',
+             name='republish',
+             arguments=[
+                 'compressed', 'raw', '--ros-args', '--remap', '/in/compressed:=/Alpha/left_camera/compressed', '--remap',
+                 'out:=' + LaunchConfiguration('namespace').perform(context) +
+                 '0/stereo_camera/left/image_color'
+                 ]),
+        Node(package='image_transport',
+             executable='republish',
+             name='republish',
+             arguments=[
+              'compressed', 'raw', '--ros-args', '--remap', '/in/compressed:=/Alpha/right_camera/compressed', '--remap',
+                 'out:=' + LaunchConfiguration('namespace').perform(context) +
+                 '0/stereo_camera/right/image_color'
+                 ]),
+        Node(package='cslam_experiments',
+             executable='publish_stereo_calibration_s3e.py',
+             name='calibration_publisher',
+             parameters=[
+                        {
+                            "robot_id": 0,
+                            "frontend.stereo_calibration_file": os.path.join(get_package_share_directory('cslam_experiments'),
+                                  'config', 's3e', 'alpha.yaml'),
+                        }
+                    ]),
+    ]
+
+
+def generate_launch_description():
+
+    return LaunchDescription([OpaqueFunction(function=launch_setup)])
