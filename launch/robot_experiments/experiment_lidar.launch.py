89d8fc1d1f4f3eb29b29bce16de83b53d7b88882
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2024-03-05 15:42:01 -0500

    Add zenoh

---
+++ launch/robot_experiments/experiment_lidar.launch.py
@@ -0,0 +1,56 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import TimerAction, OpaqueFunction, PushLaunchConfigurations, PopLaunchConfigurations, DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import launch_testing
+import launch_testing.actions
+from launch.substitutions import LaunchConfiguration
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # Params
+    cslam_proc = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("cslam_experiments"),
+                         "launch", "cslam", "cslam_lidar.launch.py")),
+        launch_arguments={
+            "config_path": os.path.join(get_package_share_directory("cslam_experiments"), "config/"),
+            "config_file": LaunchConfiguration('cslam_config_file').perform(context),
+            "robot_id": LaunchConfiguration('robot_id').perform(context),
+            "namespace": "/r" + LaunchConfiguration('robot_id').perform(context),
+            "max_nb_robots": LaunchConfiguration('max_nb_robots').perform(context),
+        }.items(),
+    )
+
+    zenoh_dds_brigde_process = ExecuteProcess(
+                        cmd=['zenoh-bridge-dds', '-d', LaunchConfiguration('robot_id').perform(context), '--allow', '/cslam/.*']
+                    )
+    
+    # Launch schedule
+    schedule = []
+
+    schedule.append(SetEnvironmentVariable('ROS_DOMAIN_ID',  LaunchConfiguration('robot_id').perform(context)))
+    schedule.append(zenoh_dds_brigde_process)
+
+    schedule.append(PushLaunchConfigurations())
+    schedule.append(cslam_proc)
+    schedule.append(PopLaunchConfigurations())
+    
+    return schedule
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        DeclareLaunchArgument('robot_id', default_value='0'),
+        DeclareLaunchArgument('max_nb_robots', default_value='5'),
+        DeclareLaunchArgument('cslam_config_file',
+                              default_value='realsense_rgbd.yaml',
+                              description=''),
+        OpaqueFunction(function=launch_setup)
+    ])
