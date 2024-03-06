e1ed542f943f6ca6f5c37a443594ecd674a1648b
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2024-03-05 21:40:44 -0500

    Remap lidar topic

--- launch/cslam/cslam_lidar.launch.py
+++ launch/cslam/cslam_lidar.launch.py
@@ -35,6 +35,9 @@ def launch_setup(context, *args, **kwargs):
                                 }
                             ],
                             prefix=LaunchConfiguration('launch_prefix_cslam'),
+                            remappings=[
+                                ('/r' + LaunchConfiguration('robot_id').perform(context) + '/pointcloud', '/points'),
+                            ],
                             namespace=LaunchConfiguration('namespace'))
 
     pose_graph_manager_node = Node(package='cslam',
